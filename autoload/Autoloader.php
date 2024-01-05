844d2e2a6c396cb477ea03842d2daf4bbc2b14fd
miranto117 <tojo.mirantoratsimbazafy@gmail.com>
2024-01-05 16:51:10 +0100

    create object autoload

--- autoload/Autoloader.php
+++ autoload/Autoloader.php
@@ -3,11 +3,11 @@
 class Autoloader{
 
     public static function register(){
-
+        spl_autoload_register(array(__CLASS__, 'autoload'));
     }
 
-    public static function Autoload(){
-
+    public static function autoload($class){
+        require __DIR__ .'/'. $class .'.php';
     }
 
 }
